ee879aaa39a6ace0831551fd05f25444419ff2e8
dan-skw <s223628@student.uek.krakow.pl>
2022-04-09 21:32:00 +0200

    add more practice kata

--- practice_sessions/09april22/phone_num.py
+++ practice_sessions/09april22/phone_num.py
@@ -2,6 +2,10 @@
 Create phone number: https://www.codewars.com/kata/525f50e3b73515a6db000b83/train/python
 
 Write a function that accepts an array of 10 integers (between 0 and 9), that returns a string of those numbers in the form of a phone number.
+
++ very clever solution, something new for me
+def create_phone_number(n):
+    return "({}{}{}) {}{}{}-{}{}{}{}".format(*n)
 """
 
 def create_phone_number(n):
